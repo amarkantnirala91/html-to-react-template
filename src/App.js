93d3438eb28af144f5b1cef1bb7c5dfd5f044a2d
amar kant <amarkantnirala2@gmail.com>
2023-10-22 15:44:34 +0530

    all final

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import { BrowserRouter , Route , Routes} from "react-router-dom";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+// import Navbar from "./components/navbar";
+// import Footer from "./components/footer";
 import HomePage from "./components/home-page";
 import About from "./components/about";
 import Service from "./components";
 import Contact from "./components/contact";
+import Layout from "./components/shared-cmp/layout";
 function App() {
   return (
        <BrowserRouter>
-        <Navbar />
+        <Layout >
         <Routes >
         <Route exact path='/' element={<HomePage />}  />
         <Route exact path='/about' element={<About />}  />
         <Route exact path='/services' element={<Service />}  />
         <Route exact path='/contact' element={<Contact />}  />
         </Routes>
-        <Footer />
+        </Layout>
      </BrowserRouter>
   );
 }
